3b45a48f20b985a8ca65a34234c85671259bfc3f
Sergey <sera200013@gmail.com>
2020-10-03 14:13:37 +0300

    Emoji menu added

--- src/components/MessageInput/index.jsx
+++ src/components/MessageInput/index.jsx
@@ -1,13 +1,41 @@
 import React from "react";
+import { Picker } from 'emoji-mart';
 
+import 'emoji-mart/css/emoji-mart.css'
 import "./MessageInput.scss"
 
 class MessagesInput extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            typedText: ""
+            typedText: "",
+            isEmojiMenuOpen: false
         }
+
+        this.insertEmoji = this.insertEmoji.bind(this);
+        this.toggleEmojiMenu = this.toggleEmojiMenu.bind(this);
+
+    }
+
+    toggleEmojiMenu() {
+        this.setState(function(prevState, props) {
+            if (prevState.isEmojiMenuOpen) {
+                document.querySelector("#emojiMenu").firstElementChild.style.display = "none";
+                return {
+                    isEmojiMenuOpen: false
+                };
+            } else {
+                document.querySelector("#emojiMenu").firstElementChild.style.display = "inline-block";
+                return {
+                    isEmojiMenuOpen: true
+                };
+            }
+
+        });
+    }
+
+    insertEmoji(e) {
+        document.querySelector(".messages-input__textbox").textContent += e.native;
     }
 
     render() {
@@ -16,8 +44,26 @@ class MessagesInput extends React.Component {
                 <div className="messages-input__attach-file">
                     <i className="far fa-file-alt" />
                 </div>
-                <span role="textbox" className="messages-input__textbox" contentEditable
-                    data-placeholder="Введите сообщение..."/>
+                <div className="messages-input__field">
+                    <div role="textbox" className="messages-input__textbox" contentEditable
+                          data-placeholder="Введите сообщение..." />
+                    <div className="messages-input__icon-wrapper">
+                        <i className="far fa-grin" onClick={this.toggleEmojiMenu} />
+                    </div>
+                    <div id="emojiMenu">
+                        <Picker theme="dark"
+                                title=""
+                                style={
+                                    {
+                                        position: 'absolute',
+                                        bottom: '25px',
+                                        right: '20px',
+                                        display: 'none'}
+                                }
+                                onSelect={this.insertEmoji}
+                        />
+                    </div>
+                </div>
                 <div className="messages-input__send">
                     <i className="far fa-paper-plane" />
                 </div>
